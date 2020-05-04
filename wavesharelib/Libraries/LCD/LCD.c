84f61fdd859c6817d2873955236f40e3bbc73a31
vasilki <lukanikhin@mail.ru>
2020-05-04 22:10:38 +0300

    add traces. sealogic shows that the data is corrected transmitted

--- wavesharelib/Libraries/LCD/LCD.c
+++ wavesharelib/Libraries/LCD/LCD.c
@@ -441,7 +441,7 @@ void lcd_init(SPI_HandleTypeDef *par_hspi)
   lcd_write_register(0x08,0x01);
   lcd_write_register(0x09,0x3F); //Row End
 
-  lcd_clear_screen(WHITE);
+  lcd_clear_screen(BLACK);
   return;
 }
 
